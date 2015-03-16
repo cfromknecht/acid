8249cd753015037a591a360e2af7411bc8c57baf
Conner Fromknecht <conner@mit.edu>
2015-03-16 05:39:00 -0400

    Fold Fibonacci + Helper methods

--- fib.lisp
+++ fib.lisp
@@ -1,7 +1,41 @@
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helper methods
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun range (max &key (min 0) (step 1))
+  (loop for n from min below max by step
+    collect n))
+
+(defun fold-left (func acc ls)
+  (if (cdr ls)
+    (fold-left func 
+               (funcall func (car ls) acc) 
+               (cdr ls))
+    acc))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Recursive Fibonacci
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; bread and butter
 (defun fib (x)
   (if (> x 1)
     (+ (fib (- x 1)) (fib (- x 2)))
     1))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fold Fibonacci
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; sum previous state to make new head, move previous head to tail
+;; (Fn-1, Fn-2) -> (Fn-1 + Fn-2, Fn-1) = (Fn, Fn-1)
+(defun nextFibState (i acc)
+  (list (apply '+ acc) (car acc)))
+
+;; updates FibState x times and returns most recently calculated Fib number
+(defun fib2 (x)
+  (car (fold-left 'nextFibState
+                  (list 1 1) 
+                  (range x)))) 
 
--- main.lisp
+++ main.lisp
@@ -1,5 +1,27 @@
 (load "fib.lisp")
 
+;; Fibonacci number to compute
+(defconstant *fib-difficulty* 42)
+
+;; displays current real and run timestamps
+(defun clock ()
+  (format t "~%[CLOCK] real: ~D run: ~D~%~%" (get-internal-real-time)
+                                             (get-internal-run-time)))
+
+;; calculates the n'th Fibonacci number and prints the result
+(defun print-fib2 (n)
+  (format t ">   (fib2 ~D)~%    ~D~%" n (fib2 n)))
+
+;; test Fibonacci performance
 (defun main ()
-  (format t "~D~%" (fib 20)))
+  (progn
+    (clock)
+    (format t ">   (fib ~D)~%    ~D~%" *fib-difficulty* (fib *fib-difficulty*))
+    (clock)
+    (print-fib2 *fib-difficulty*)
+    (clock)
+    (print-fib2 100)
+    (clock)
+    (print-fib2 1000)
+    (clock)))
 
